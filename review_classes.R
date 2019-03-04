59e830d27334c6695c970af3be18577ac860b081
Romain Ragonnet <romain.ragonnet@monash.edu>
2019-03-04 16:13:29 +1100

    Initial commit

---
+++ review_classes.R
@@ -0,0 +1,274 @@
+library(R6)
+source('G:/My Drive/R_toolkit/graph_tools.R')
+
+Cohort <- R6Class(
+  "Cohort",
+  public = list(
+    author = '',
+    smear_status = '',
+    cohort_name = '',
+    year_range = NULL,
+    cohort_size = 0,
+    times = c(),
+    perc_death = NULL,
+    perc_alive = NULL,
+    formatted_data = NULL,
+  
+    initialize = function(author, smear_status, cohort_name, year_range,
+                          cohort_size, times, perc_death, perc_alive){
+      self$author = author
+      self$smear_status = smear_status
+      self$cohort_name = cohort_name
+      self$year_range = year_range
+      self$cohort_size = cohort_size
+      self$times = times
+      self$perc_death = perc_death
+      self$perc_alive = perc_alive
+      
+
+      # work with death percentages only
+      if (is.null(self$perc_death)){
+        self$perc_death = 100 - self$perc_alive
+        self$perc_alive = NULL
+      }
+      
+      # check
+      if (length(self$times) != length(self$perc_death)){
+        print("Warning: times and survival data do not have same length")
+      }
+      
+      # add the data for t=0
+      if (!(0 %in% self$times)){
+        self$times = c(0, self$times)
+        if (is.null(self$perc_death)){
+          self$perc_alive = c(100, self$perc_alive)
+        }else{
+          self$perc_death = c(0, self$perc_death)
+        }
+      }
+      
+      # format the data
+      if (!is.null(self$cohort_size)){
+        self$format_data()
+        
+      }
+    },
+    
+    format_data = function(){
+      n_rows = length(self$times) - 1
+      u = rep(NA,n_rows)
+      formatted_data = data.frame('t_start'=u, 'delta_t'=u, 'n_at_risk'=u, 'n_new_deaths'=u)
+      
+      for (i in 1:n_rows){  # i represent the index of the starting time
+        formatted_data$t_start[i] = self$times[i]
+        formatted_data$delta_t[i] = self$times[i+1] - self$times[i]
+        formatted_data$n_at_risk[i] = round((1 - self$perc_death[i]/100) * self$cohort_size)
+        formatted_data$n_new_deaths[i] = round((self$perc_death[i+1] - self$perc_death[i])*self$cohort_size/100)
+        
+        if (formatted_data$n_at_risk[i] < formatted_data$n_new_deaths[i]){
+          print("WARNING: more dead persons than at_risk persons")
+        }
+      }
+      self$formatted_data = formatted_data
+      
+    },
+    
+    plot_cohort_data = function(){
+      title = paste(self$author, self$smear_status, self$cohort_name)
+      if (is.null(self$perc_death)){
+        plot(self$times, self$perc_alive, main=title, 
+             xlab='time (years)', ylab='percentage still alive')
+      }else{
+        plot(self$times, self$perc_death, main=title,
+             xlab='time (years)', ylab='percentage dead')
+      }
+    },
+    
+    get_plot_color = function(){
+      color=list(
+        "Baart De La Faille" = my_blue,
+        "Buhl" = my_red,
+        "Griep" = my_purple,
+        "Tattersall" = my_gold,
+        "Thompson" = my_brown,
+        "Hartley" = 'black',
+        "Braeuning" = my_green,
+        "Backer" = my_grey,
+        "Trail" = 'blue',
+        "Sinding-Larsen" = 'purple',
+        "Furth" = 'green',
+        "Berg" = 'yellow',
+        "Munchbach" = "orange"
+      )
+      # determine an alpha between 0.5 and 1
+      # 0.5 for the oldest times (1905), 1 for the most recent (1940) 
+      alpha = (mean(self$year_range) - 1905)/35
+      print(alpha)
+      add.alpha(color[[self$author]], alpha)      
+    }
+  )
+)
+
+Analysis <- R6Class(
+    "Analysis",
+    public = list(
+      cohorts = c(),
+     
+      add_cohort = function(author, smear_status, cohort_name, year_range,
+                            cohort_size, times, perc_death, perc_alive){
+        cohort = Cohort$new(author, smear_status, cohort_name, year_range,
+                            cohort_size, times, perc_death, perc_alive)
+        self$cohorts = c(self$cohorts, cohort)
+      },
+     
+      plot_cohort_dates = function(){
+        n_coh = length(self$cohorts)
+        line_height = 1.8
+        filename = 'outputs/dates'
+        open_figure(filename, 'png', w=14, h=7)
+        
+        par(mar=c(5.1,12,3,1))
+        
+        plot(0,0,type='n',xlab='years', ylab='',
+             xlim=c(1900,1950), ylim=c(0,(n_coh+1)*line_height), axes = FALSE)
+        axis(side=1)
+        
+        cpt=0
+        for (cohort in self$cohorts){
+          cpt = cpt+1
+          col='black'
+          if (cohort$smear_status == 'negative'){
+            col='gray'
+          }
+          lines(cohort$year_range, c(cpt*line_height, cpt*line_height), lwd=4, col=col)
+          name = paste(cohort$author, cohort$cohort_name, sep=' ')
+          mtext(text = name,side = 2,line = 0,at = cpt*line_height, las=1)
+          
+          text(x = 1900, y=cpt*line_height,labels = cohort$cohort_size, pos=4)
+        }
+        text(x=1900, y=cpt*line_height+2, labels='N', pos=4)
+        dev.off()
+      },
+      
+      plot_multi_cohort = function(smear_status=c('positive', 'negative'), 
+                                   plot_model=FALSE, model=1, mu=NA, mu_t=NA, gamma=NA,kappa=NA, alpha=NA){
+        xmax = 0
+        cohorts_to_plot = c()
+            
+        # populate cohort_to_plot and determine xmax
+        for (coh in self$cohorts){
+          if (coh$smear_status %in% smear_status){
+            xmax = max(xmax, max(coh$times))
+            cohorts_to_plot = c(cohorts_to_plot, coh)
+          }
+        }
+        
+        filename = 'outputs/all_cohorts'
+        title = ''
+        if (length(smear_status)<2){
+          filename = paste(filename, smear_status[1], sep="_")
+        }
+        if (plot_model){
+          title = paste('Model',model,sep=' ')
+          filename = paste(filename, "with_model", model, sep="_")
+        }
+        open_figure(filename, 'png', w=12, h=9)
+        plot(0,0,xlim=c(0,xmax), ylim=c(0,100), xlab='time (years)',
+             ylab='death %', main=title)
+        count = 0
+        for (coh in cohorts_to_plot){
+          count = count+1
+          color = coh$get_plot_color()
+          points(coh$times, coh$perc_death, col=color, pch=18, cex=1.5)
+          lines(coh$times, coh$perc_death, col=color, lwd=3)
+        }
+        if (plot_model){
+          t = seq(0,30,by=0.01)
+          if (model == 1){
+            death = self$death_proportion_model_1(t=t, mu=mu, mu_t=mu_t, gamma=gamma)
+          }else if (model == 2){
+            death = self$death_proportion_model_2(t=t, mu=mu, mu_t=mu_t, gamma=gamma,kappa=kappa, alpha=alpha)
+          }
+          lines(t, 100*death, col='black', lwd=4)
+        }
+        dev.off()
+      },
+      death_proportion_model_1 = function(t, mu, mu_t, gamma){
+        A = (mu+mu_t)/(mu+mu_t+gamma) - mu*gamma/((mu_t+gamma)*(mu+mu_t+gamma))
+        B = 1 - exp(-(mu+mu_t+gamma)*t)
+        C = mu*gamma/(mu*(mu_t+gamma))
+        D = 1 - exp(-mu*t)
+        death = A*B + C*D
+        return(death)
+      },
+      death_proportion_model_2 = function(t, mu, mu_t, gamma, kappa, alpha){
+        theta = gamma + kappa + mu + mu_t
+        w = gamma + mu + alpha*mu_t
+        Q = gamma*( (theta-w-kappa)/((theta-mu)*(theta-w)) + kappa/((w-mu)*(theta-w)) )
+        
+        A = 1-kappa/(theta-w) - gamma*(theta-w-kappa)/((theta-mu)*(theta-w))
+        B = kappa/(theta-w) - gamma*kappa/((w-mu)*(theta-w))
+        
+        death = 1 - (A*exp(-theta*t) + B*exp(-w*t) + Q*exp(-mu*t))
+        return(death)
+      },
+      square_dist_model_to_cohort = function(mu, mu_t, gamma, cohort, model=1, kappa=NA, alpha=NA){
+        square_dist = 0
+        for (i in 1:length(cohort$times)){
+          if (model == 1){
+            modelled_death = self$death_proportion_model_1(cohort$times[i], mu, mu_t, gamma)
+          }else if(model==2){
+            modelled_death = self$death_proportion_model_2(cohort$times[i], mu, mu_t, gamma, kappa, alpha)
+          }
+          square_dist = square_dist + (modelled_death - cohort$perc_death[i]/100)**2
+        }
+        return(square_dist)
+      },
+      square_dist_model_to_all_data = function(mu, mu_t, gamma, kappa=NA, alpha=NA, smear_status='positive', model=1){
+        square_dist = 0
+        for (cohort in self$cohorts){
+          if (cohort$smear_status %in% smear_status){
+            square_dist = square_dist + self$square_dist_model_to_cohort(mu, mu_t, gamma, cohort, model=model,
+                                                                       kappa=kappa, alpha=alpha)
+          }
+        }
+        return(square_dist)
+      },
+      optimise_fit = function(mu=1/55, smear_status='positive', model=1){
+        
+        if (model == 1){
+          fun_to_minimise = function(params){
+            mu_t = params[1]
+            gamma = params[2]
+            dist = self$square_dist_model_to_all_data(mu,mu_t,gamma,smear_status,model = 1)
+            return(dist)
+          }  
+          par_0 = c(0.23, 0.1)  # initial guess for mu_t and gamma
+          lower_bounds = c(0, 0)
+        }else if(model == 2){
+          fun_to_minimise = function(params){
+            mu_t = params[1]
+            gamma = params[2]
+            kappa = params[3]
+            alpha = params[4]
+            dist = self$square_dist_model_to_all_data(mu, mu_t, gamma, kappa=kappa, alpha=alpha, smear_status=smear_status, model=2)
+            return(dist)
+          }  
+          par_0 = c(0.23, 0.1, 1, 0.5)  # initial guess for mu_t and gamma
+          lower_bounds = c(0, 0, 0, 0)
+        }
+        opt = optim(par = par_0, fn = fun_to_minimise, lower=lower_bounds)
+        mu_t = opt$par[1]
+        gamma = opt$par[2]
+        kappa = NA
+        alpha = NA
+        if (model == 2){
+          kappa = opt$par[3]
+          alpha = opt$par[4]
+        }
+        self$plot_multi_cohort(smear_status=smear_status, plot_model=TRUE, model=model, mu=mu,
+                               mu_t=mu_t, gamma=gamma, kappa=kappa, alpha=alpha)
+        return(opt)
+      }
+    )
+)
---
+++ review_run.R
@@ -0,0 +1,469 @@
+setwd('C:/Users/rrag0004/Models/tb_natural_history/')
+source('review_classes.R')
+
+analysis = Analysis$new()
+
+
+#_____________________________________________________________
+#  Baart de la faille
+
+#     smear_pos
+analysis$add_cohort(author = 'Baart De La Faille',
+                    smear_status = 'positive',
+                    cohort_name = '22_25',
+                    year_range = c(1922, 1925),
+                    cohort_size = 177,
+                    times = c(2, 4, 6, 8, 10),
+                    perc_death = c(43, 56, 64, 68, 71),
+                    perc_alive = NULL
+)
+
+analysis$add_cohort(author = 'Baart De La Faille',
+                    smear_status = 'positive',
+                    cohort_name = '26_29',
+                    year_range = c(1926, 1929),
+                    cohort_size = 149,
+                    times = c(2, 4, 6),
+                    perc_death = c(51, 61, 63),
+                    perc_alive = NULL
+)
+
+analysis$add_cohort(author = 'Baart De La Faille',
+                    smear_status = 'positive',
+                    cohort_name = '30_33',
+                    year_range = c(1930, 1933),
+                    cohort_size = 125,
+                    times = c(2, 4),
+                    perc_death = c(55, 64),
+                    perc_alive = NULL
+)
+
+analysis$add_cohort(author = 'Baart De La Faille',
+                    smear_status = 'positive',
+                    cohort_name = '34_35',
+                    year_range = c(1934, 1935),
+                    cohort_size = 83,
+                    times = c(2),
+                    perc_death = c(43),
+                    perc_alive = NULL
+)
+
+
+#_________________
+#     smear_pos then smear_neg
+analysis$add_cohort(author = 'Baart De La Faille',
+                    smear_status = 'negative',
+                    cohort_name = '22_25',
+                    year_range = c(1922, 1925),
+                    cohort_size = 103,
+                    times = c(2, 4, 6, 8, 10),
+                    perc_death = c(13, 22, 27, 30, 34),
+                    perc_alive = NULL
+)
+
+analysis$add_cohort(author = 'Baart De La Faille',
+                    smear_status = 'negative',
+                    cohort_name = '26_29',
+                    year_range = c(1926, 1929),
+                    cohort_size = 110,
+                    times = c(2, 4, 6),
+                    perc_death = c(3, 6, 13),
+                    perc_alive = NULL
+)
+
+analysis$add_cohort(author = 'Baart De La Faille',
+                    smear_status = 'negative',
+                    cohort_name = '30_33',
+                    year_range = c(1930, 1933),
+                    cohort_size = 220,
+                    times = c(2, 4),
+                    perc_death = c(5, 11),
+                    perc_alive = NULL
+)
+
+analysis$add_cohort(author = 'Baart De La Faille',
+                    smear_status = 'negative',
+                    cohort_name = '34_35',
+                    year_range = c(1934, 1935),
+                    cohort_size = 164,
+                    times = c(2),
+                    perc_death = c(2),
+                    perc_alive = NULL
+)
+
+#_________________
+#     smear_neg
+analysis$add_cohort(author = 'Baart De La Faille',
+                    smear_status = 'negative',
+                    cohort_name = '22_25_allneg',
+                    year_range = c(1922, 1925),
+                    cohort_size = NULL,
+                    times = c(2, 4, 6),
+                    perc_death = c(5, 7, 10),
+                    perc_alive = NULL
+)
+
+analysis$add_cohort(author = 'Baart De La Faille',
+                    smear_status = 'negative',
+                    cohort_name = '26_29_allneg',
+                    year_range = c(1926, 1929),
+                    cohort_size = NULL,
+                    times = c(2, 4, 6),
+                    perc_death = c(6, 9, 12),
+                    perc_alive = NULL
+)
+
+analysis$add_cohort(author = 'Baart De La Faille',
+                    smear_status = 'negative',
+                    cohort_name = '30_33_allneg',
+                    year_range = c(1930, 1933),
+                    cohort_size = NULL,
+                    times = c(2, 4),
+                    perc_death = c(5, 7),
+                    perc_alive = NULL
+)
+
+analysis$add_cohort(author = 'Baart De La Faille',
+                    smear_status = 'negative',
+                    cohort_name = '34_35_allneg',
+                    year_range = c(1934, 1935),
+                    cohort_size = NULL,
+                    times = c(2),
+                    perc_death = c(3),
+                    perc_alive = NULL
+)
+
+
+#_____________________________________________________________
+#  Buhl
+
+analysis$add_cohort(author = 'Buhl',
+                    smear_status = 'positive',
+                    cohort_name = '25_29',
+                    year_range = c(1925, 1929),
+                    cohort_size = 314,
+                    times = seq(1,10,by = 1),
+                    perc_death = NULL,
+                    perc_alive = c(72, 60, 56, 47, 45, 39, 38, 37, 35, 32)
+)
+
+#_____________________________________________________________
+#  Griep
+
+analysis$add_cohort(author = 'Griep',
+                    smear_status = 'positive',
+                    cohort_name = '20_25',
+                    year_range = c(1920, 1925),
+                    cohort_size = 372,
+                    times = c(5, 10, 15),
+                    perc_death = c(52, 67, 75),
+                    perc_alive = NULL
+)
+
+analysis$add_cohort(author = 'Griep',
+                    smear_status = 'positive',
+                    cohort_name = '25_30',
+                    year_range = c(1925, 1930),
+                    cohort_size = 603,
+                    times = c(5, 10),
+                    perc_death = c(51, 67),
+                    perc_alive = NULL
+)
+
+
+#_____________________________________________________________
+#  Tattersall
+analysis$add_cohort(author = 'Tattersall',
+                    smear_status = 'positive',
+                    cohort_name = '',
+                    year_range = c(1914, 1940),
+                    cohort_size = 1192,
+                    times = seq(1,31,by=1),
+                    perc_death = NULL,
+                    perc_alive = c(71.1, 55.2, 44.2, 37, 32.1, 29.7, 26.9,
+                                   25.1, 23.6, 22, 21.5, 19.9, 19.1, 18.2,
+                                   17.2, 16.9, 16.2, 15.5, 15.5, 14.8, 14.8,
+                                   14.5, 14.5, 14.5, 13.9, 13.9, 13.9, 12.9, 12.9, 12.9, 12.9)
+)
+
+
+#_____________________________________________________________
+#  Thompson
+
+analysis$add_cohort(author = 'Thompson',
+                    smear_status = 'positive',
+                    cohort_name = '',
+                    year_range = c(1928, 1940),
+                    cohort_size = 406,
+                    times = seq(1,10,by = 1),
+                    perc_death = NULL,
+                    perc_alive = c(60, 43.2, 36.7, 30.5, 26.8, 22.3, 20.3, 18.9, 17.2, 13.9)
+)
+
+
+#_____________________________________________________________
+#  Hartley (from Berg Table 12)
+mean_men = (406*58+585*55+517*55+408*54+255*52+151*56+60*43)/2382
+analysis$add_cohort(author = 'Hartley',
+                    smear_status = 'positive',
+                    cohort_name = 'Men',
+                    year_range = c(1905, 1914),
+                    cohort_size = 2382,
+                    times = c(5),
+                    perc_death = NULL,
+                    perc_alive = c(mean_men)
+)
+mean_women = (200*65 + 294*69 + 227*65 + 119*61 + 64*67 + 30*63 + 10*80)/944
+analysis$add_cohort(author = 'Hartley',
+                    smear_status = 'positive',
+                    cohort_name = 'Women',
+                    year_range = c(1905, 1914),
+                    cohort_size = 944,
+                    times = c(5),
+                    perc_death = NULL,
+                    perc_alive = c(mean_women)
+)
+
+#_____________________________________________________________
+#  Braeuning (from Berg, Table 3)
+
+# function used to convert rates to percentages for Table 3 in Berg
+convert_rates_to_perc_alive = function(rates){
+  perc_alive = rep(0, length(rates))
+  for (year in 1:length(rates)){
+    if (year ==1){
+      perc_alive[year] = 100 - rates[year]
+    }else if(year<6){
+      perc_alive[year] = perc_alive[year-1] * (1-rates[year]/100)
+    }else{
+      perc_alive[year] = perc_alive[year-1] * (1-rates[year]/100)**5
+    }
+  }
+  return(perc_alive)
+}
+
+rates = c(39, 31, 21, 17, 12, 6)
+perc_alive_Braeuning = convert_rates_to_perc_alive(rates)
+
+analysis$add_cohort(author = 'Braeuning',
+                    smear_status = 'positive',
+                    cohort_name = '',
+                    year_range = c(1920, 1921),
+                    cohort_size = 607,
+                    times = c(1,2,3,4,5,10),
+                    perc_death = NULL,
+                    perc_alive = perc_alive_Braeuning
+)
+
+#_____________________________________________________________
+#  Backer (from Berg, Table 3)
+rates_men = c(23.1, 20.2, 16.4, 14.5, 13.3, 9.3, 6.3, 3.7)
+rates_women = c(30.3, 24.3, 18.5, 16.1, 11.8, 9.7, 5.6, 4.5)
+perc_alive_Backer_men = convert_rates_to_perc_alive(rates_men)
+perc_alive_Backer_women = convert_rates_to_perc_alive(rates_women)
+
+analysis$add_cohort(author = 'Backer',
+                    smear_status = 'positive',
+                    cohort_name = 'Men',
+                    year_range = c(1911, 1920),
+                    cohort_size = 1172,
+                    times = c(1,2,3,4,5,10,15,20),
+                    perc_death = NULL,
+                    perc_alive = perc_alive_Backer_men
+)
+
+analysis$add_cohort(author = 'Backer',
+                    smear_status = 'positive',
+                    cohort_name = 'Women',
+                    year_range = c(1911, 1920),
+                    cohort_size = 1140,
+                    times = c(1,2,3,4,5,10,15,20),
+                    perc_death = NULL,
+                    perc_alive = perc_alive_Backer_women
+)
+
+#_____________________________________________________________
+#  Trail (from Berg, Table 3)
+rates_men = c(13.1, 14, 13.5, 12.9, 10.3, 7.6)
+rates_women = c(12.3, 16.7, 13.9, 12.1, 10.4, 7.6)
+perc_alive_Trail_men = convert_rates_to_perc_alive(rates_men)
+perc_alive_Trail_women = convert_rates_to_perc_alive(rates_women)
+
+analysis$add_cohort(author = 'Trail',
+                    smear_status = 'positive',
+                    cohort_name = 'Men',
+                    year_range = c(1911, 1928),
+                    cohort_size = 1671,
+                    times = c(1,2,3,4,5,10),
+                    perc_death = NULL,
+                    perc_alive = perc_alive_Trail_men
+)
+
+analysis$add_cohort(author = 'Trail',
+                    smear_status = 'positive',
+                    cohort_name = 'Women',
+                    year_range = c(1911, 1928),
+                    cohort_size = 944,
+                    times = c(1,2,3,4,5,10),
+                    perc_death = NULL,
+                    perc_alive = perc_alive_Trail_women
+)
+
+#_____________________________________________________________
+#  Sinding-Larsen (from Berg, Table 3)
+rates = c(32.3, 9.1, 9.1, 7.3, 7.3, 3.8)
+perc_alive_Sinding= convert_rates_to_perc_alive(rates)
+
+analysis$add_cohort(author = 'Sinding-Larsen',
+                    smear_status = 'positive',
+                    cohort_name = '',
+                    year_range = c(1906, 1932),
+                    cohort_size = 1114,
+                    times = c(1,2,3,4,5,10),
+                    perc_death = NULL,
+                    perc_alive = perc_alive_Sinding
+)
+
+
+#_____________________________________________________________
+#  Furth (from Berg, Table 3)
+analysis$add_cohort(author = 'Furth',
+                    smear_status = 'positive',
+                    cohort_name = '12_15',
+                    year_range = c(1912, 1915),
+                    cohort_size = 303,
+                    times = c(3, 5, 10),
+                    perc_death = c(54.8, 70, 82.5),
+                    perc_alive = NULL
+)
+
+analysis$add_cohort(author = 'Furth',
+                    smear_status = 'positive',
+                    cohort_name = '16_19',
+                    year_range = c(1916, 1919),
+                    cohort_size = 307,
+                    times = c(3, 5, 10),
+                    perc_death = c(61.6, 72.3, 81.8),
+                    perc_alive = NULL
+)
+
+analysis$add_cohort(author = 'Furth',
+                    smear_status = 'positive',
+                    cohort_name = '20_24',
+                    year_range = c(1920, 1924),
+                    cohort_size = 386,
+                    times = c(3, 5),
+                    perc_death = c(53.1, 67.9),
+                    perc_alive = NULL
+)
+
+
+#_____________________________________________________________
+#  Berg (Using Nico's spreadsheet)
+
+analysis$add_cohort(author = 'Berg',
+                    smear_status = 'positive',
+                    cohort_name = '',
+                    year_range = c(1928, 1934),
+                    cohort_size = 2042,
+                    times = c(1,3,5,10),
+                    perc_death = NULL,
+                    perc_alive = c(69.3, 50.9, 41.8, 29.4)
+)
+
+#_____________________________________________________________
+#  Munchbach (Using Berg, Table 11)
+
+analysis$add_cohort(author = 'Munchbach',
+                    smear_status = 'positive',
+                    cohort_name = 'men_20_21',
+                    year_range = c(1920, 1921),
+                    cohort_size = 266,
+                    times = c(2,3,5),
+                    perc_death = c(30.1, 38, 48.1),
+                    perc_alive = NULL
+)
+
+analysis$add_cohort(author = 'Munchbach',
+                    smear_status = 'positive',
+                    cohort_name = 'men_22_23',
+                    year_range = c(1922, 1923),
+                    cohort_size = 681,
+                    times = c(2,3,5),
+                    perc_death = c(32.8, 38.8, 48.6),
+                    perc_alive = NULL
+)
+
+analysis$add_cohort(author = 'Munchbach',
+                    smear_status = 'positive',
+                    cohort_name = 'men_24_25',
+                    year_range = c(1924, 1925),
+                    cohort_size = 504,
+                    times = c(2, 3, 5),
+                    perc_death = c(33.9, 42.9, 46.6),
+                    perc_alive = NULL
+)
+
+analysis$add_cohort(author = 'Munchbach',
+                    smear_status = 'positive',
+                    cohort_name = 'men_26_27',
+                    year_range = c(1926, 1927),
+                    cohort_size = 660,
+                    times = c(2),
+                    perc_death = c(31.4),
+                    perc_alive = NULL
+)
+
+analysis$add_cohort(author = 'Munchbach',
+                    smear_status = 'positive',
+                    cohort_name = 'women_20_21',
+                    year_range = c(1920, 1921),
+                    cohort_size = 428,
+                    times = c(2, 3, 5),
+                    perc_death = c(44.9, 52.6, 61.2),
+                    perc_alive = NULL
+)
+
+analysis$add_cohort(author = 'Munchbach',
+                    smear_status = 'positive',
+                    cohort_name = 'women_22_23',
+                    year_range = c(1922, 1923),
+                    cohort_size = 497,
+                    times = c(2, 3, 5),
+                    perc_death = c(40.6, 49.1, 54.7),
+                    perc_alive = NULL
+)
+
+analysis$add_cohort(author = 'Munchbach',
+                    smear_status = 'positive',
+                    cohort_name = 'women_24_25',
+                    year_range = c(1924, 1925),
+                    cohort_size = 433,
+                    times = c(2, 3, 5),
+                    perc_death = c(30.0, 36.7, 39.0),
+                    perc_alive = NULL
+)
+
+analysis$add_cohort(author = 'Munchbach',
+                     smear_status = 'positive',
+                     cohort_name = 'women_26_27',
+                     year_range = c(1926, 1927),
+                     cohort_size = 497,
+                     times = c(2),
+                     perc_death = c(32.2),
+                     perc_alive = NULL
+)
+
+#______________________________________________________________-
+# RUNNING methods
+# analysis$plot_multi_cohort(plot_model=FALSE)
+#analysis$plot_multi_cohort(plot_model=TRUE)
+
+#analysis$plot_multi_cohort(smear_status = c('positive'), plot_model=FALSE)
+#analysis$plot_multi_cohort(smear_status = c('negative'), plot_model=FALSE)
+
+#analysis$plot_multi_cohort(smear_status = c('positive'), plot_model=TRUE)
+#analysis$plot_multi_cohort(smear_status = c('negative'), plot_model=TRUE)
+
+# analysis$plot_cohort_dates()
+
