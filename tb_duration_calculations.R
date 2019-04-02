8dd2b03020c426ba53de6dc192711e29c18a3a0d
Romain Ragonnet <romain.ragonnet@monash.edu>
2019-04-02 11:30:19 +1100

    Simple calculations of TB durations

---
+++ tb_duration_calculations.R
@@ -0,0 +1,31 @@
+# fixed params
+tsr = 0.8
+tx_duration = 0.5
+mu = 1/70
+
+# variable cdr
+cdr = seq(0.5,0.9,by = 0.0001)
+
+# natural history (old)
+recovery_sp_old = 0.15
+mu_t_sp_old = 0.15
+
+recovery_sn_old = 0.13
+mu_t_sn_old = 0.018
+
+
+# natural history (new)
+recovery_sp = 0.19
+mu_t_sp = 0.325
+
+recovery_sn = 0.13
+mu_t_sn = 0.018
+
+duration_sp = 1/(recovery_sp + mu + mu_t_sp + cdr*tsr/tx_duration)
+duration_sn = 1/(recovery_sn + mu + mu_t_sn + cdr*tsr/tx_duration)
+duration = 0.5*(duration_sn + duration_sp)
+
+ymax = max(duration_sn)
+
+plot(cdr, duration, type='l',ylim=c(0,ymax))
+lines(cdr, duration_sn, col='gray')
